cb51e73aa5ed7be6c4aca417f9d7f72525a85e6e
nattyKhoe <natasha.kusuma@outlook.com>
2022-05-29 19:40:38 +1000

    commit tree dc0f9cc065f1590dff046adf2a4abad0d672cf7e

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (name, greeting){
+    let message =[];
+    for (let i=0; i<name.length; i++){
+        message[i] = `Thank you, ${name[i]}, for the wonderful ${greeting} gift!`;
+    }
+    return message;
+}
+
+function countDown(number){
+    while (number>=0) {
+        console.log(number);
+        number--;
+    }
+}
